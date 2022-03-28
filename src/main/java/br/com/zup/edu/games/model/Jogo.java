03c228f2fc03e07035119feac792329786a6167d
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-03-28 16:24:29 -0300

    Criando entidade Jogo

---
+++ src/main/java/br/com/zup/edu/games/model/Jogo.java
@@ -0,0 +1,37 @@
+package br.com.zup.edu.games.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Jogo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private String link;
+
+    public Jogo(String nome, String descricao, String link) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.link = link;
+    }
+
+    @Deprecated
+    /**
+     * @deprecated Construtor para uso exclusivo do Hibernate
+     */
+    public Jogo() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
