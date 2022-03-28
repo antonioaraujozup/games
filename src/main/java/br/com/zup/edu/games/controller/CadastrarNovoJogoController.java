62b08265e50d4d216a964fbb016e9dfd7efe7397
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-03-28 16:39:44 -0300

    Criando método para cadastrar games

--- src/main/java/br/com/zup/edu/games/controller/CadastrarNovoJogoController.java
+++ src/main/java/br/com/zup/edu/games/controller/CadastrarNovoJogoController.java
@@ -1,8 +1,16 @@
 package br.com.zup.edu.games.controller;
 
+import br.com.zup.edu.games.model.Jogo;
 import br.com.zup.edu.games.repository.JogoRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/games")
@@ -12,6 +20,19 @@ public class CadastrarNovoJogoController {
 
     public CadastrarNovoJogoController(JogoRepository repository) {
         this.repository = repository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> cadastrar (@RequestBody @Valid JogoRequest request, UriComponentsBuilder uriComponentsBuilder) {
+
+        Jogo jogo = request.paraJogo();
+
+        repository.save(jogo);
+
+        URI location = uriComponentsBuilder.path("/games/{id}").buildAndExpand(jogo.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+
     }
 
 }
